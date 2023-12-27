c79f4dc221d92f213a98ab370c5063d1a39f5b97
Deathronik <bemavadim@gmail.com>
2023-12-27 12:51:36 +0200

    Add a progress bar and fix the issue with the 429 response status by adding timeouts and retries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Form, Table, Button, Spinner} from "react-bootstrap";
+import {Button, Form, ProgressBar, Spinner, Table} from "react-bootstrap";
 import React, {useState} from "react";
 import Result from "./Components/Result/Result";
 import Creator from "./Components/Creator/Creator";
@@ -7,18 +7,15 @@ import Creator from "./Components/Creator/Creator";
 function App() {
     const [wallets, setWallets] = useState([])
     const [results, setResults] = useState([])
+    const [progress, setProgress] = useState(0)
     const [total, setTotal] = useState(0)
     const [isLoading, setIsLoading] = useState(false)
 
-    const onCLickCheckHandler = async () => {
-        setIsLoading(true)
-        const walletsStats = []
-        let total = 0
+    const fetchData = async (wallet) => {
+        await new Promise(r => setTimeout(r, 250))
 
-        for (const wallet of wallets) {
-            if (wallet === "") continue
-
-            const response = await fetch(`https://airdrop-api.jup.ag/allocation/${wallet}`, {
+        try {
+            return await fetch(`https://airdrop-api.jup.ag/allocation/${wallet}`, {
                 "headers": {
                     "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
                     "sec-ch-ua-mobile": "?0",
@@ -27,11 +24,26 @@ function App() {
                 "referrer": "https://airdrop.jup.ag/",
                 "referrerPolicy": "strict-origin-when-cross-origin",
                 "body": null,
-                "method": "GET",
-                "mode": "cors",
-                "credentials": "omit"
-            });
+                "method": "GET"
+            })
+        } catch (e) {
+            if (e instanceof TypeError && e.message === "Failed to fetch") {
+                await new Promise(r => setTimeout(r, 10000))
+                return await fetchData(wallet);
+            }
+        }
+    }
+    const onCLickCheckHandler = async () => {
+        setProgress(0)
+        setIsLoading(true)
+        const walletsStats = []
+        let total = 0
 
+        for (const wallet of wallets) {
+            await setProgress(prevState => prevState + 1)
+            if (wallet === "") continue
+
+            const response = await fetchData(wallet)
             const responseJSON = await response.json().catch(() => null);
 
             if (responseJSON) {
@@ -65,6 +77,7 @@ function App() {
                                   style={{width: 500, height: 300, resize: "none"}} as="textarea"/>
                 </Form>
                 <Button className="w-25" onClick={onCLickCheckHandler}>{isLoading ? <Spinner animation="grow" size="sm"/> : "Check"}</Button>
+                {isLoading && <ProgressBar className="mt-3" animated now={progress} max={wallets.length} />}
             </div>
             <div className="mt-3">
                 <Creator/>
